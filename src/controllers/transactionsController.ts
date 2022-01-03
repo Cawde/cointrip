63dea7aeb032d1225e8adefbdbbb9c89c6de8099
cawde <winfreycory28@gmail.com>
2022-01-02 21:22:16 -0600

    added stripe checkout functionality

--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { Transaction } from "../models/transactions";
+import { Transaction } from '../models/transactions';
+const { STRIPE_KEY } = process.env;
 const {
   createTransaction,
   getAllTransactions
 } = require('../models/transactions');
+const stripe = require('stripe')( STRIPE_KEY );
+const { uuidv4 } = require('uuid');
+
 
 
 async function getAllTransactions_get (req:Request, res:Response, next:NextFunction) {
@@ -31,7 +35,48 @@ async function createTransaction_post (req:Request, res:Response, next:NextFunct
   }
 }
 
+async function checkout_post(req: Request, res: Response, next: NextFunction) {
+  const { token, name, amount, notes } = req.body
+  try {
+    const customer = await stripe.customers.create({
+      name: name,
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuidv4();
+
+    const charge = await stripe.charges.create({
+      amount: amount * 100,
+      currency: "usd",
+      customer: customer.id,
+      receipt_email: token.email,
+      description: notes,
+      shipping: {
+        name: token.card.name,
+        address: {
+          line1: token.card.address_line1,
+          line2: token.card.address_line2,
+          city: token.card.address_city,
+          country: token.card.address_country, 
+          postal_code: token.card.address_zip
+        }
+      }
+    }, {
+      idempotency_key
+    });
+
+    console.log("Charge: ", { charge });
+    res.send({message: "Success"})
+
+  } catch (e) {
+    console.error(e);
+    next({message: "Failure"})
+  }
+}
+
 module.exports = {
   getAllTransactions_get,
-  createTransaction_post
+  createTransaction_post,
+  checkout_post
 }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,7 +2,7 @@ import { NextFunction } from "express";
 
 const express = require('express');
 const transactionsRouter = express.Router();
-const { getAllTransactions_get, createTransaction_post } = require('../controllers/transactionsController');
+const { getAllTransactions_get, createTransaction_post, checkout_post } = require('../controllers/transactionsController');
 
 transactionsRouter.use((req: any, res:any, next:NextFunction) => {
   console.log('A request is being made to /transactions');
@@ -11,5 +11,6 @@ transactionsRouter.use((req: any, res:any, next:NextFunction) => {
 
 transactionsRouter.get('/', getAllTransactions_get);
 transactionsRouter.post('/', createTransaction_post);
+transactionsRouter.post('/checkout', checkout_post);
 
 module.exports = transactionsRouter;
